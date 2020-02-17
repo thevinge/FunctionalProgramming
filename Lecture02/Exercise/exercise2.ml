21f8e7a2ce64569bf013e57352972d0f26806e56
JV <jv@JV-XPS.localdomain>
2020-02-17 12:59:56 +0100

    Lecture 2 Exercise almost done

--- Lecture02/Exercise/exercise2.ml
+++ Lecture02/Exercise/exercise2.ml
@@ -26,11 +26,11 @@ let _ = QCheck_runner.run_tests ~verbose:true  [sum_concat_test;]
 
 
 let rec merge_sort lst1 lst2 = match lst1, lst2 with
-    | [],list -> lst2
-    | list,[] -> lst1
+    | [],_ -> lst2
+    | _,[] -> lst1
     | elem1::elems1, elem2::elems2 -> if elem1 <= elem2 
-                                then elem1:: merge_sort elems1 (elem2::elems2)
-                                else elem2:: merge_sort elems2 (elem1::elems1)
+                                then elem1:: merge_sort elems1 lst2
+                                else elem2:: merge_sort elems2 lst1
 
 
 let sort_concat_test =
@@ -39,4 +39,52 @@ let sort_concat_test =
     (fun (xs,ys) ->  merge_sort (List.sort compare xs) (List.sort compare ys) = List.sort compare (xs@ys))
 
 
-let a = QCheck_runner.run_tests ~verbose:true  [sort_concat_test;]
+let _ = QCheck_runner.run_tests ~verbose:true  [sort_concat_test;]
+
+
+let int64_Back =
+  Test.make ~name:"int64 to int and back" ~count: 1000
+    (int)
+    (fun (i) -> i = (Int64.to_int (Int64.of_int i)) )
+
+let int32_Back =
+  Test.make ~name:"int32 to int and back" ~count: 1000
+    (int)
+    (fun (i) -> i = (Int32.to_int (Int32.of_int i)) )
+
+let intString_Back =
+  Test.make ~name:"String to int and back" ~count: 1000
+    (int)
+    (fun (i) -> i = (int_of_string (string_of_int i)) )
+
+
+let _ = QCheck_runner.run_tests ~verbose:true  [int64_Back; int32_Back; intString_Back;]
+
+(** Euclid **)
+let rec eu_gcd = fun n m ->
+    if (m = 0)
+    then m
+    else 
+    if n > m
+    then eu_gcd (n-m) m
+    else eu_gcd (m-n) n;;
+
+
+let rec gcd a b = 
+    let r = a mod b in
+    if r = 0 then b else gcd b r;;
+
+
+
+let rec fib n = match n with
+    | 0 -> 1
+    | 1 -> 1
+    | n -> fib(n-1) + fib(n-2);;
+
+
+(**  Fibonacci
+if n < 2 then 1 else
+if n = accum then num1 + num2 else
+fib2 n (num1 + num2) num1 (accum+1)
+**)
+
