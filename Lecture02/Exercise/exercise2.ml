0c1a97ae635fc52b9037347cf48ca9f588b1fb43
JV <jv@JV-XPS.localdomain>
2020-02-17 12:14:51 +0100

    Lecture 02 Exercise

--- Lecture02/Exercise/exercise2.ml
+++ Lecture02/Exercise/exercise2.ml
@@ -1,3 +1,4 @@
+open QCheck
 
 let first tuple = match tuple with
     | (x,y) -> x;;
@@ -15,3 +16,27 @@ let rec member stack element = match stack with
     | elem::elems -> if elem = element
                      then true
                      else member elems element
+
+let sum_concat_test =
+  Test.make ~name:"Split and Sum" ~count: 1000
+    (pair (list int) (list int))
+    (fun (xs,ys) ->  sum(xs @ ys) = (sum ys) + (sum xs));;
+
+let _ = QCheck_runner.run_tests ~verbose:true  [sum_concat_test;]
+
+
+let rec merge_sort lst1 lst2 = match lst1, lst2 with
+    | [],list -> lst2
+    | list,[] -> lst1
+    | elem1::elems1, elem2::elems2 -> if elem1 <= elem2 
+                                then elem1:: merge_sort elems1 (elem2::elems2)
+                                else elem2:: merge_sort elems2 (elem1::elems1)
+
+
+let sort_concat_test =
+  Test.make ~name:"Split and sort" ~count: 1000
+    (pair (list int) (list int))
+    (fun (xs,ys) ->  merge_sort (List.sort compare xs) (List.sort compare ys) = List.sort compare (xs@ys))
+
+
+let a = QCheck_runner.run_tests ~verbose:true  [sort_concat_test;]
