4040ef61f3655d00c1c132c813cd9d66eb608049
JV <jv@JV-XPS.localdomain>
2020-02-17 14:54:42 +0100

    In Class Exercise (Interpreter)

--- Lecture03/exercise/exercise3.ml
+++ Lecture03/exercise/exercise3.ml
@@ -45,3 +45,46 @@ let rec size ae = match ae with
     | Times (ae0, ae1) -> 
         1 + size(ae0) + size(ae1)
 
+
+
+(* a datatype of abstract machine instructions *)
+type inst =
+  | Load
+  | Push of int
+  | Add
+  | Mult
+
+(* our compiler from arithmetic expressions to instructions *)
+let rec compile ae = match ae with
+  | X -> [Load]
+  | Lit i -> [Push i]
+  | Plus (ae0, ae1) ->
+    let is0 = compile ae0 in
+    let is1 = compile ae1 in
+    is0 @ is1 @ [Add]
+  | Times (ae0, ae1) ->
+    let is0 = compile ae0 in
+    let is1 = compile ae1 in
+    is0 @ is1 @ [Mult]
+
+(*  compile mytree;;  *)
+
+
+exception Darn_no_Stack_Element_Add
+exception Darn_no_Stack_Element_Multi
+
+let rec run xreg inst stack = match inst with
+    | [] -> stack
+    | e::elems -> run xreg elems (match e with 
+        | Load -> [xreg] @ stack
+        | Push i -> [i] @ stack
+        | Add -> (match stack with
+            | [] -> []
+            | stackElem::stackElems -> (match stackElems with
+                | [] -> raise Darn_no_Stack_Element_Add
+                | e1::remainder -> [(stackElem + e1)] @ remainder))
+        | Mult -> (match stack with
+            | [] -> []
+            | stackElem::stackElems -> (match stackElems with
+                | [] -> raise Darn_no_Stack_Element_Multi
+                | e2::remainder -> [(stackElem * e2)] @ remainder))) 
