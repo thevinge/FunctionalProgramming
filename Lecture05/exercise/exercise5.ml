889437e50d02770141bb01bd4c91a85b6cf287ce
JV <jv@JV-XPS.localdomain>
2020-03-02 14:55:16 +0100

    Lecture 5 Exercise 1

--- Lecture05/exercise/exercise5.ml
+++ Lecture05/exercise/exercise5.ml
@@ -1,1 +1,28 @@
 open QCheck
+
+
+(* Exercise 1*)
+
+module H1 =
+struct
+  type cmd =
+    | Add of string * int
+    | Remove of string
+    | Find of string
+    | Mem of string [@@deriving show]
+end
+
+module H2 =
+struct
+  type cmd =
+    | Add of string * int
+    | Remove of string
+    | Find of string
+    | Mem of string [@@deriving show { with_path = false }]
+end
+
+(* Use the above like this: H1.shows_cmd (H1.Add ("test", 2)) *)
+
+(* End Exercise 1*)
+
+
